c6310681cbbaea228390f57e17ae7100c2706172
takuto-yamamoto <tyguitar0v0627@gmail.com>
2024-03-07 00:52:38 +0900

    update: define schema

---
+++ api/schemas/task.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode: True
+
+
+class TaskCreateRequest(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskBase):
+    id: int
+
+    class Config:
+        orm_mode: True
+
+
+class TaskDelete(TaskBase):
+    id: int
